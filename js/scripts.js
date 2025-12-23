bd5b416b0c23f9256bf082ebe1353392418da738
charlesgraczcki <charlesgraczcki@gmail.com>
2025-12-23 10:02:20 -0300

    comitando para ajustar no note mesmo

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
-const verProjetosBtn = null;
-const nula =null
+//seleção de elementos
+const clicked = document.querySelector(".cliqueAqui");
+
+
+
+
+
+
+
+//funções
+    clicked.addEventListener("click", ()=>{
+    clicked.classList.toggle("escondido")
+    });
+
